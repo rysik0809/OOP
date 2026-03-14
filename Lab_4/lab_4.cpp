1c85426a2bdcaf269f158370f21527e146470357
rysik0809 <rysikpupsik904@gmail.com>
2026-03-15 00:33:20 +0300

    готовый отчет и поправка кода

--- Lab_4/lab_4.cpp
+++ Lab_4/lab_4.cpp
@@ -2,87 +2,89 @@
 #include <vector>
 #include <algorithm>
 
+using namespace std;
+
 template<typename T>
 class Set {
-    private:
-        std::vector<T> elements; 
+private:
+    vector<T> elements;
 
-    public:
-        Set() = default;
+public:
+    Set() = default;
 
-        Set(std::initializer_list<T> list) {
-            for (const T& item : list) {
-                add(item);
+    Set(initializer_list<T> list) {
+        for (const T& item : list) {
+            add(item);
+        }
+    }
+
+    void add(const T& item) {
+        if (!contains(item)) {
+            elements.push_back(item);
+        }
+    }
+
+    bool contains(const T& item) const {
+        return find(elements.begin(), elements.end(), item) != elements.end();
+    }
+
+    void remove(const T& item) {
+        auto it = find(elements.begin(), elements.end(), item);
+        if (it != elements.end()) {
+            elements.erase(it);
+        }
+    }
+
+    size_t size() const {
+        return elements.size();
+    }
+
+    Set operator-(const T& item) const {
+        Set result = *this;
+        result.remove(item);
+        return result;
+    }
+
+    Set operator*(const Set& other) const {
+        Set result;
+        for (const T& item : elements) {
+            if (other.contains(item)) {
+                result.add(item);
             }
         }
+        return result;
+    }
 
-        void add(const T& item) {
-            if (!contains(item)) {
-                elements.push_back(item);
+    bool operator<(const Set& other) const {
+        for (const T& item : elements) {
+            if (!other.contains(item)) {
+                return false;
             }
         }
+        return size() < other.size();
+    }
 
-        bool contains(const T& item) const {
-            return std::find(elements.begin(), elements.end(), item) != elements.end();
+    friend ostream& operator<<(ostream& os, const Set& set) {
+        os << "{ ";
+        for (size_t i = 0; i < set.elements.size(); ++i) {
+            os << set.elements[i];
+            if (i + 1 < set.elements.size()) os << ", ";
         }
-
-        void remove(const T& item) {
-            auto it = std::find(elements.begin(), elements.end(), item);
-            if (it != elements.end()) {
-                elements.erase(it);
-            }
-        }
-
-        size_t size() const {
-            return elements.size();
-        }
-
-        Set operator-(const T& item) const {
-            Set result = *this; 
-            result.remove(item);
-            return result;
-        }
-
-        Set operator*(const Set& other) const {
-            Set result;
-            for (const T& item : elements) {
-                if (other.contains(item)) {
-                    result.add(item);
-                }
-            }
-            return result;
-        }
-
-        bool operator<(const Set& other) const {
-            for (const T& item : elements) {
-                if (!other.contains(item)) {
-                    return false; 
-                }
-            }
-            return size() < other.size();
-        }
-
-        friend std::ostream& operator<<(std::ostream& os, const Set& set) {
-            os << "{ ";
-            for (size_t i = 0; i < set.elements.size(); ++i) {
-                os << set.elements[i];
-                if (i + 1 < set.elements.size()) os << ", ";
-            }
-            os << " }";
-            return os;
-        }
+        os << " }";
+        return os;
+    }
 };
 
 template<typename T>
-Set<T> inputSet(const std::string& prompt) {
-    std::cout << prompt;
+Set<T> inputSet(const string& prompt) {
+    cout << prompt;
     int n;
-    std::cin >> n;
+    cin >> n;
     Set<T> set;
-    std::cout << "Введите " << n << " элементов(а): ";
+    cout << "Введите " << n << " элементов(а): ";
     for (int i = 0; i < n; ++i) {
         T item;
-        std::cin >> item;
+        cin >> item;
         set.add(item);
     }
     return set;
@@ -92,22 +94,22 @@ int main() {
     Set<int> setA = inputSet<int>("Введите размер первого множества: ");
     Set<int> setB = inputSet<int>("Введите размер второго множества: ");
 
-    std::cout << "\nМножество A: " << setA << std::endl;
-    std::cout << "Множество B: " << setB << std::endl;
+    cout << "\nМножество A: " << setA << endl;
+    cout << "Множество B: " << setB << endl;
 
-    std::cout << "\n--- Операции ---\n";
+    cout << "\n--- Операции ---\n";
 
     int elem;
-    std::cout << "Введите элемент для удаления из A (setA - elem): ";
-    std::cin >> elem;
+    cout << "Введите элемент для удаления из A (setA - elem): ";
+    cin >> elem;
     Set<int> setA_minus = setA - elem;
-    std::cout << "A - " << elem << " = " << setA_minus << std::endl;
+    cout << "A - " << elem << " = " << setA_minus << endl;
 
     Set<int> intersection = setA * setB;
-    std::cout << "A * B (пересечение) = " << intersection << std::endl;
+    cout << "A * B (пересечение) = " << intersection << endl;
 
-    std::cout << "A < B (A строгое подмножество B)? " << (setA < setB ? "да" : "нет") << std::endl;
-    std::cout << "B < A (B строгое подмножество A)? " << (setB < setA ? "да" : "нет") << std::endl;
+    cout << "A < B (A строгое подмножество B)? " << (setA < setB ? "да" : "нет") << endl;
+    cout << "B < A (B строгое подмножество A)? " << (setB < setA ? "да" : "нет") << endl;
 
     return 0;
 }
